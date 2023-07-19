df366dd07b26142a0450990c9c59606d1e4484c9
NataliaJeszke <nataliajeszke8@gmail.com>
2023-07-19 20:20:39 +0200

    description

--- src/app/utils/showTags.ts
+++
@@ -1,9 +0,0 @@
-export class showTags {
-    [key: string]: any;
-    constructor(title?:string, subject?:string, date?:string, description?:string) {
-        this.title = title;
-        this.subject = subject;
-        this.date = date;
-        this.description = description;
-    }
-}
