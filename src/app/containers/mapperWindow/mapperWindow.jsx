9bc400f0d697389b874f6337dcb441dda2920007
NataliaJeszke <nataliajeszke8@gmail.com>
2023-07-19 15:41:58 +0200

    added identifier and modified removal function

--- src/app/containers/mapperWindow/mapperWindow.jsx
+++ src/app/containers/mapperWindow/mapperWindow.jsx
@@ -4,7 +4,9 @@ import { useState } from "react";
 import { useDrop } from "react-dnd";
 import { useDrag } from "react-dnd";
 
-export default function MapperWindow(tagValue, source) {
+let tagIdCounter = 0;
+
+export default function MapperWindow({ tagValue, source }) {
   const [leftBoard, setLeftBoard] = useState([]);
   const [rightBoard, setRightBoard] = useState([]);
 
@@ -20,7 +22,13 @@ export default function MapperWindow(tagValue, source) {
   }));
 
   const addTagToBoard = (id, source) => {
-    const newTag = { id: id, tagKey: id, tagValue: id, source: source };
+    const newTag = {
+      id: id,
+      tagKey: id,
+      tagValue: id,
+      source: source,
+      identifier: `tag-${tagIdCounter++}`,
+    };
 
     if (source === "left") {
       setLeftBoard((leftBoard) => [...leftBoard, newTag]);
@@ -30,38 +38,39 @@ export default function MapperWindow(tagValue, source) {
   };
 
   const updateBoard = (id, source) => {
-
     if (source === "left") {
       setLeftBoard((leftBoard) => {
-      const newArray = [...leftBoard];
-      const index = newArray.findIndex((element) => element.id === id);
-  
-      if (index !== -1) {
-        const element = newArray.splice(index, 1)[0]; 
-        newArray.push(element);
-      }
-  
-      return newArray;
-    });
-  } else if (source === "right") {
-    setRightBoard((rightBoard) => {
-      const newArray = [...rightBoard];
-      const index = newArray.findIndex((element) => element.id === id);
+        const newArray = [...leftBoard];
+        const index = newArray.findIndex((element) => element.id === id);
 
-      if (index !== -1) {
-        const element = newArray.splice(index, 1)[0];
-        newArray.push(element);
-      }
+        if (index !== -1) {
+          const element = newArray.splice(index, 1)[0];
+          newArray.push(element);
+        }
 
-      return newArray;
-    });
-  }
-}
+        return newArray;
+      });
+    } else if (source === "right") {
+      setRightBoard((rightBoard) => {
+        const newArray = [...rightBoard];
+        const index = newArray.findIndex((element) => element.id === id);
 
-  const removeTagFromBoard = (id, source) => {
+        if (index !== -1) {
+          const element = newArray.splice(index, 1)[0];
+          newArray.push(element);
+        }
+
+        return newArray;
+      });
+    }
+  };
+
+  const removeTagFromBoard = (identifier, source) => {
     if (source === "left") {
       setLeftBoard((leftBoard) => {
-        const index = leftBoard.findIndex((tag) => tag.id === id);
+        const index = leftBoard.findIndex(
+          (tag) => tag.identifier === identifier
+        );
         if (index !== -1) {
           leftBoard.splice(index, 1);
           if (index < rightBoard.length) {
@@ -76,12 +85,14 @@ export default function MapperWindow(tagValue, source) {
       });
     } else if (source === "right") {
       setRightBoard((rightBoard) => {
-        const index = rightBoard.findIndex((tag) => tag.id === id);
+        const index = rightBoard.findIndex(
+          (tag) => tag.identifier === identifier
+        );
         if (index !== -1) {
           rightBoard.splice(index, 1);
           if (index < leftBoard.length) {
-            setLeftBoard((rightBoard) => {
-              const updatedLeftBoard = [...rightBoard];
+            setLeftBoard((leftBoard) => {
+              const updatedLeftBoard = [...leftBoard];
               updatedLeftBoard.splice(index, 1);
               return updatedLeftBoard;
             });
@@ -96,35 +107,38 @@ export default function MapperWindow(tagValue, source) {
     type: "tag",
     item: { id: tagValue, source: source },
     collect: (monitor) => ({
-      isDragging: !!monitor.isDragging()
-    })
+      isDragging: !!monitor.isDragging(),
+    }),
   }));
 
   return (
-    <div className="bg-white rounded-lg basis-1/2 flex-auto flex-wrap shadow-lg" ref={drop}>
+    <div
+      className="bg-white rounded-lg basis-1/2 flex-auto flex-wrap shadow-lg"
+      ref={drop}
+    >
       <h2 className="text-center m-2">Mapping Window</h2>
       <div className="flex gap-10 justify-center">
         <div ref={drag}>
           <h2 className="m-2">Your Tag</h2>
-          {leftBoard.map((tag) => (
+          {leftBoard.map((tag, index) => (
             <Tag
-              key={tag.id}
+              key={tag.identifier}
               tagKey={tag.tagKey}
               tagValue={tag.tagValue}
               source={tag.source}
-              onRemove={removeTagFromBoard}
+              onRemove={() => removeTagFromBoard(tag.identifier, tag.source)}
             />
           ))}
         </div>
         <div ref={drag}>
-          <h2 className="m-2">Mapp To</h2>
-          {rightBoard.map((tag) => (
+          <h2 className="m-2">Map To</h2>
+          {rightBoard.map((tag, index) => (
             <Tag
-              key={tag.id}
+              key={tag.identifier}
               tagKey={tag.tagKey}
               tagValue={tag.tagValue}
               source={tag.source}
-              onRemove={removeTagFromBoard}
+              onRemove={() => removeTagFromBoard(tag.identifier, tag.source)}
             />
           ))}
         </div>
